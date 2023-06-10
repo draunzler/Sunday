ea42e0185ba894a2889e1db402072201054a3e8c
Draunzler <shadymeister2017@gmail.com>
2023-06-10 21:20:13 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ app.post('/', async (req,res) => {
             bot: response.data.choices[0].text
         })
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).send({ error })
     }
 })
