a8f7027edb883be34fb08682cd083820678e53b6
Draunzler <shadymeister2017@gmail.com>
2023-06-10 22:49:17 +0530

    last commit

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-50ja.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
