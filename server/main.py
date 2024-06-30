7e6001d824ff99b946a5d361763109a032c16d16
draunzler <shadymeister2017@gmail.com>
2024-06-30 05:38:34 +0530

    added lang chain and the forget api

--- server/main.py
+++ server/main.py
@@ -9,6 +9,10 @@ import openai
 import os
 import logging
 import uvicorn
+from langchain.chains import LLMChain
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -28,8 +32,23 @@ client = OpenAI(
 # Initialize OpenAI API
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+genai_api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 gemini_model = genai.GenerativeModel('gemini-1.5-flash')
+google_llm = GoogleGenerativeAI(api_key=genai_api_key, model="gemini-1.5-flash")
+
+prompt = PromptTemplate(
+    input_variables=["history", "input"],
+    template="The following is a conversation with an AI assistant named Sunday. The assistant is helpful, creative, clever, and very friendly.\n\n{history}\nHuman: {input}\nAI:"
+)
+
+memory = ConversationBufferMemory(memory_key="history")
+
+llm_chain = LLMChain(
+    llm=google_llm,
+    prompt=prompt,
+    memory=memory
+)
 
 logger.info("main.py is being executed")
 
@@ -71,12 +90,23 @@ async def generate_response(prompt_request: PromptRequest):
 
         #     return content
         # else:
-        response = gemini_model.generate_content(prompt)
-        logger.info(f"{response.text}")
-        return {"bot": response.text}
+        # response = gemini_model.generate_content(prompt)
+        response = llm_chain.predict(input=prompt)
+        logger.info(f"{response}")
+        return {"bot": response}
     except Exception as e:
         logger.error(f"Error generating response: {e}")
         raise HTTPException(status_code=500, detail="Something went wrong")
+    
+@app.post("/forget")
+async def forget_memory():
+  try:
+    memory.clear()
+
+    return {"message": "Memory cleared successfully"}
+  except Exception as e:
+    logger.error(f"Error clearing memory: {e}")
+    raise HTTPException(status_code=500, detail="Something went wrong")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
